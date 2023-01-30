231d6a72fda3cd4770f38625f793e861514823eb
Sontr0n <sontronow@gmail.com>
2023-01-29 23:20:50 -0800

    Adding Volume Slider

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,7 +15,18 @@ TutorialAudioProcessorEditor::TutorialAudioProcessorEditor (TutorialAudioProcess
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize (200, 200);
+
+    //these define the parameters of our slider object 
+    midiVolume.setSliderStyle(juce::Slider::LinearBarVertical);
+    midiVolume.setRange(0.0, 127.0, 1.0);
+    midiVolume.setTextBoxStyle(juce::Slider::NoTextBox, false, 90, 0);
+    midiVolume.setPopupDisplayEnabled(true, false, this);
+    midiVolume.setTextValueSuffix("Volume");
+    midiVolume.setValue(1.0);
+
+    //this function adds the slider to the editor
+    addAndMakeVisible(&midiVolume);
 }
 
 TutorialAudioProcessorEditor::~TutorialAudioProcessorEditor()
@@ -30,11 +41,13 @@ void TutorialAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("Midi Volume", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void TutorialAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    midiVolume.setBounds(40, 30, 20, getHeight() - 60);
+    setResizable(true, true);
 }
